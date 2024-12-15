f94bdd233f60caa91c91886c3788b9268d29b424
andiupdates <andiupdates@gmail.com>
2024-12-15 02:07:41 +0000

    push

--- toggle.js
+++
@@ -1,8 +0,0 @@
-const themeToggle = document.getElementById('theme-toggle');
-
-themeToggle.addEventListener('change', () => {
-  document.body.classList.toggle('dark-mode');
-});
-
-// Dark mode styles
-document.body.classList.add('dark-mode');
